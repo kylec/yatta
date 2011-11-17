a6c47b2075bc322fe1d0b78450d350ef054369d5
kylec <kylelyk02@gmail.com>
2011-11-16 21:46:31 -0600

    added goal xml builder

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,18 +1,36 @@
 class GoalsController < ApplicationController
+  require 'builder'
+  
   def new
-    #@goal = Goal.new
     @title = "Create Goal"
   end
 
   def create
-    puts "XXXXXXXXXXXXXXXxx"
-    puts params[:newgoal]["0"][:title]
-    puts params[:newgoal]["0"][:description]
-    puts params[:newgoal]["1"][:title]
-    puts params[:newgoal]["1"][:description]
-    puts params[:newgoal]["2"][:title]
-    puts params[:newgoal]["2"][:description]
-    render 'new'
+    goal_title = params[:newgoal][:goal_title]
+    goal_description = params[:newgoal][:goal_description]  
+    xml_path = "/Users/kylito/"
+    xml_file = "#{goal_title}-#{current_user.id}.xml"
+    file = File.new("#{xml_path}#{xml_file}", "w")
+    xml = Builder::XmlMarkup.new :target => file  
+    xml.instruct!
+    xml.goal_title(goal_title)
+    xml.goal_description(goal_description)
+    numMilestones = params[:newgoal].size - 2
+    numMilestones.times do |i| 
+      xml.milestone{ 
+        xml.title(params[:newgoal]["#{i}"][:title])
+        xml.description(params[:newgoal]["#{i}"][:description])
+      }
+    end
+    file.close  
+    
+    @goal = current_user.goals.build(:schedule => xml_file)
+    if @goal.save
+      flash[:success] = "Goal created!"
+      redirect_to root_path
+    else
+      render 'pages/home'
+    end
   end
 
   def destroy
