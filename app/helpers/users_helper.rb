4d8dc473d5787943f925e8ecc14911410a401d1e
kylec <kylelyk02@gmail.com>
2012-06-09 17:47:10 -0500

    change bg color. add horizontal menu bar and profile div

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,11 @@
 module UsersHelper
   def profilePicture
     if @user.profile_picture == NIL then
-      picturePath = "Cropped - Solid.png"
+      picturePath = "UserProfileImages/1.jpg"
     else
       picturePath = @user.profile_picture
     end
     
-    image_tag(picturePath, :alt =>"Profile Picture", :class => "round", :width => "160", :height => "160")
+    image_tag(picturePath, :alt =>"Profile Picture", :width => "160", :height => "200")
   end
 end
