bf5c840f49a5bffddd43907a2aedaa3c75084e78
Albert Dam <alut266@hotmail.com>
2012-06-16 18:17:48 -0500

    Turn goal page buttons to links

--- app/controllers/user_goal_relationships_controller.rb
+++ app/controllers/user_goal_relationships_controller.rb
@@ -2,7 +2,7 @@ class UserGoalRelationshipsController < ApplicationController
   before_filter :authenticate
 
   def create
-    @goal = Goal.find(params[:user_goal_relationship][:goal_id])
+    @goal = Goal.find_by_id(params[:goal_id])
     current_user.work!(@goal)
     respond_to do |format|
       format.html { redirect_to user_goal_path(current_user.username, @goal.title) }
